092451fdeb14bf5ff77873829e70e1acb3f29cb5
CristianLMartinez <cristian.lm@globant.com>
2024-07-18 10:00:32 -0500

    try to run mvn clean test with jacoco and cucumber

--- src/test/java/com/cristian/tiusers/CucumberTest.java
+++ src/test/java/com/cristian/tiusers/CucumberTest.java
@@ -1,14 +1,20 @@
 package com.cristian.tiusers;
 
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@Suite
+@IncludeEngines("cucumber")
 @RunWith(Cucumber.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CucumberOptions(
-        features = "src/test/resources",
+        features = "src/test/resources/features",
         glue = {"com.cristian.tiusers.steps", "com.cristian.tiusers"}
 )
 public class CucumberTest {
